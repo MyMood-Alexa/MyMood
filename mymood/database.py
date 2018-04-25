e0760ee0c47a8511696e5ff3504a964ce9c7f5ba
Darren <dxddarren@gmail.com>
2018-04-24 20:55:47 -0700

    database access + create table fx

---
+++ mymood/database.py
@@ -0,0 +1,40 @@
+import boto3
+import datetime
+
+boto3.setup_default_session(region_name='us-west-1') 
+dynamodb = boto3.resource("dynamodb") 
+table = dynamodb.Table("Interactions")
+date = datetime.date.today()
+
+def create_table():
+    #TODO need key to differentiate users
+    table = dynamodb.create_table(
+        TableName='Interactions',
+        KeySchema=[
+            {
+                'AttributeName': 'date',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'time',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'date',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'time',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+        )
+    print("Table status:", table.table_status)
+    
+    
