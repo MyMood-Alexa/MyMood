5ab61ec2652377edc0ce735a370699264cb1cc1a
Darren <dxddarren@gmail.com>
2018-05-04 20:36:26 -0700

    Interaction object model addded

---
+++ mymood/Interaction.py
@@ -0,0 +1,23 @@
+
+
+class Interaction:
+    
+    
+    def __init__(self, session_id=None, time=None, responses=[]):
+        self.session_id = session_id
+        self.responses = responses
+        self.time = time
+    
+        
+    def update_response(self, response):
+        self.responses.append(response)
+        
+    
+    def print_interaction(self):
+        print("Session: ", self.session_id)
+        print("Time : ", self.time)
+        for x in self.responses:
+            print(x)
+            
+        print()
+        
